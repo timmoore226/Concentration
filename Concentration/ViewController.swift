a43a16be6fe00712fce1701bdbbdd76b3f7c4cc5
Tim Moore <timmoore226@gmail.com>
2018-07-30 19:38:45 -0400

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Timothy Moore on 7/30/18.
+//  Copyright © 2018 Tim Moore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
